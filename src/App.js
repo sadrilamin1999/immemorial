eda0706f931b16d69edbf38fae32c42a4c4c05fc
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-09-15 02:00:20 +0600

    smooth scroll added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { useSmoothSscroll } from "./hooks/smoothScroll";
 
 const App = () => {
+  useSmoothSscroll();
   return (
     <div>
       <Navbar />
