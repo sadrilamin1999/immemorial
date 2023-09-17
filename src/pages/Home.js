6a3ac9c04139ddfe47b3e5dafdae8b17d0ba3ed4
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-09-17 17:47:24 +0600

    about section done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
-import Fetured from "../components/Fetured";
 import Hero from "../components/Hero";
+import Fetured from "../components/Fetured";
+import About from "../components/About";
 
 const Home = () => {
   return (
     <div className="home px-[4.7rem]">
       <Hero />
       <Fetured />
+      <About />
     </div>
   );
 };
