5908ac83b1ca602c63dde1caf03f70ae2c6a9cd4
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-09-18 22:59:35 +0600

    project done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -12,9 +12,10 @@ const Blog = () => {
   return (
     <section className="wrapper ">
       <SectionTitle titleText="Blog" />
-      <div className="bolg-wrapper grid grid-cols-[40%_auto] gap-[5vw]">
+      <div className="bolg-wrapper grid grid-cols-[40%_auto] gap-[10rem]">
         <div className="blog-left h-full w-full flex items-center">
           <img
+            className="h-full w-full"
             ref={imgRef}
             src="https://images.pexels.com/photos/16269008/pexels-photo-16269008/free-photo-of-young-woman-in-sunglasses-and-headscarf-leaning-against-coppery-wall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
@@ -24,13 +25,13 @@ const Blog = () => {
           <h2 className="text-[6vw] font-baijamjuree font-[500] tracking-wider text-custom-primary">
             Lorem ipsum.
           </h2>
-          <p className="text-[2.5vw] leading-[1.5]">
+          <p className="text-[2vw] leading-[1.5]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
             veniam mollitia perferendis consectetur atque, odio ipsa ullam
             commodi consequatur dolorem quis ipsam, error perspiciatis unde
             dignissimos vel illo. Error, consequuntur?
           </p>
-          <p className="text-[2.5vw] leading-[1.5]">
+          <p className="text-[2vw] leading-[1.5]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
             ad necessitatibus pariatur, labore laboriosam officia dignissimos
             excepturi accusamus nulla.
